71bf119f19a807df89865639de15ba8e41d6c291
heka1024 <heka1024@gmail.com>
2021-02-09 00:11:16 +0900

    add repository and application.properties

---
+++ src/test/java/com/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.springboot.web.domain.posts;
+
+import com.example.springboot.domain.posts.Posts;
+import com.example.springboot.domain.posts.PostsRepository;
+import com.example.springboot.web.HelloController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "게시글";
+        String content = "본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("heka1024@snu.ac.kr")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+
+
+}
